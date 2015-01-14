b043d53acef2d104e9cb3f75830218f14025d3e9
Philip Hughes <philip@h4w5.com>
2015-01-14 16:33:15 -0500

    working on fb oauth using gem

--- config.ru
+++ config.ru
@@ -1,6 +1,8 @@
 require 'sinatra/base'
 require 'sinatra/reloader'
 
+require 'facebook_oauth'
+
 require_relative 'server'
 
 run Futbook::Server
--- server.rb
+++ server.rb
@@ -9,7 +9,28 @@ module Futbook
     end
 
     get "/" do
+      session[:client] = FacebookOAuth::Client.new(
+        :application_id     => ENV["FACEBOOK_OAUTH_ID"],
+        :application_secret => ENV["FACEBOOK_OAUTH_SECRET"],
+        :callback           => 'http://localhost:9292/facebook/callback'
+      )
+      @facebook_oauth_link = session[:client].authorize_url
       render :erb, :index
     end
+
+    get "/facebook/callback" do
+      session[:client].authorize(:code => params["code"])
+      user_info = session[:client].me.info
+
+      binding.pry
+
+      redirect to("/test")
+    end
+
+    get "/test" do
+      render :erb, :index
+    end
+
+
   end
 end
