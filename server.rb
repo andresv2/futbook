b23af28afa1c89316f2b9aa32ccd9a7c40a7cb23
Philip Hughes <philip@h4w5.com>
2015-01-14 17:16:03 -0500

    fb login now works, but app set up wrong n fb

--- server.rb
+++ server.rb
@@ -9,26 +9,36 @@ module Futbook
     end
 
     get "/" do
-      session[:client] = FacebookOAuth::Client.new(
+      client = FacebookOAuth::Client.new(
         :application_id     => ENV["FACEBOOK_OAUTH_ID"],
         :application_secret => ENV["FACEBOOK_OAUTH_SECRET"],
         :callback           => 'http://localhost:9292/facebook/callback'
       )
-      @facebook_oauth_link = session[:client].authorize_url
+
+      @facebook_oauth_link = client.authorize_url
       render :erb, :index
     end
 
     get "/facebook/callback" do
-      session[:client].authorize(:code => params["code"])
-      user_info = session[:client].me.info
+      client = FacebookOAuth::Client.new(
+        :application_id     => ENV["FACEBOOK_OAUTH_ID"],
+        :application_secret => ENV["FACEBOOK_OAUTH_SECRET"],
+        :callback           => 'http://localhost:9292/facebook/callback'
+      )
 
-      binding.pry
+      access_token = client.authorize(:code => params["code"])
+      user_info    = client.me.info
 
-      redirect to("/test")
+      user_info["timezone"] = user_info["timezone"].to_s
+      user_info["verified"] = user_info["verified"].to_s
+      session[:access_token] = access_token.token
+      session[:user]         = user_info
+
+      redirect to("/players/" + session["user"]["id"])
     end
 
-    get "/test" do
-      render :erb, :index
+    get "/players/:id" do
+      render :erb, :profile
     end
 
 
